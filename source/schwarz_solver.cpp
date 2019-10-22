ae0d01374ec8a99d568cfef517b181e9523bf9f5
soumyadipghosh <soumyadip93@gmail.com>
2019-10-22 19:00:33 -0400

    test commit

--- source/schwarz_solver.cpp
+++ source/schwarz_solver.cpp
@@ -31,7 +31,7 @@ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ******************************<SCHWARZ LIB LICENSE>*************************/
 
-//This is branch event-based
+//This is the branch event-based
 
 #include <schwarz/config.hpp>
 

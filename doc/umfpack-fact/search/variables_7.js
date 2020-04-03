6d79ea08c7d24b121b89d078eccd29046d7ae550
GitHub Action <action@github.com>
2020-04-03 09:34:25 +0000

    Update documentation from 9d3fa40

--- doc/umfpack-fact/search/variables_7.js
+++ doc/umfpack-fact/search/variables_7.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['matrix_5ffilename',['matrix_filename',['../structSchwarzWrappers_1_1Settings.html#a734222e1021c3e01e6a004e08f081026',1,'SchwarzWrappers::Settings']]],
   ['max_5fiters',['max_iters',['../structSchwarzWrappers_1_1Metadata.html#a78f04f4b7c58c9260d7e79aac6f10cda',1,'SchwarzWrappers::Metadata']]],
   ['metis_5fobjtype',['metis_objtype',['../structSchwarzWrappers_1_1Settings.html#abbfee6b651293973f478e70ae656d6d0',1,'SchwarzWrappers::Settings']]],
   ['min_5fresidual_5fnorm',['min_residual_norm',['../structSchwarzWrappers_1_1Metadata.html#a536b647d0c8ec4328e46583a53fa19b3',1,'SchwarzWrappers::Metadata']]],

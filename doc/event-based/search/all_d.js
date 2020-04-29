9eb1873b989346245b935a18281e7a76893120e5
GitHub Action <action@github.com>
2020-04-29 20:26:15 +0000

    Update documentation from 36d9ec0

--- doc/event-based/search/all_d.js
+++ doc/event-based/search/all_d.js
@@ -13,7 +13,7 @@ var searchData=
   ['setup_5fcomm_5fbuffers',['setup_comm_buffers',['../classschwz_1_1Communicate.html#ad77bbc19111a7b6ff79962d3a659a2cc',1,'schwz::Communicate::setup_comm_buffers()'],['../classschwz_1_1SolverRAS.html#a167b67ab0fb9d46244e9de563a682b43',1,'schwz::SolverRAS::setup_comm_buffers()']]],
   ['setup_5fglobal_5fmatrix',['setup_global_matrix',['../classschwz_1_1Initialize.html#af655816bbed181e0a243efa7e35e942f',1,'schwz::Initialize']]],
   ['setup_5flocal_5fmatrices',['setup_local_matrices',['../classschwz_1_1Initialize.html#ad24764a4ded54c2af6a5111ba8c8228f',1,'schwz::Initialize::setup_local_matrices()'],['../classschwz_1_1SolverRAS.html#aebbdd245b7019af802606ad95a8e3a5a',1,'schwz::SolverRAS::setup_local_matrices()']]],
-  ['setup_5fvectors',['setup_vectors',['../classschwz_1_1Initialize.html#a299af15376e7453c5bc94251f8a66251',1,'schwz::Initialize']]],
+  ['setup_5fvectors',['setup_vectors',['../classschwz_1_1Initialize.html#a3a94510247a1d4035780daf6a1d679cd',1,'schwz::Initialize']]],
   ['setup_5fwindows',['setup_windows',['../classschwz_1_1Communicate.html#ab41e8e19e90ce2d53234cbec2cdb3b38',1,'schwz::Communicate::setup_windows()'],['../classschwz_1_1SolverRAS.html#a8cd640aa073c0890d6121dc3afb3eaac',1,'schwz::SolverRAS::setup_windows()']]],
   ['shifted_5fiter',['shifted_iter',['../structschwz_1_1Settings.html#a33a9382f1b5961a8cb2133cb0c22daf0',1,'schwz::Settings']]],
   ['solve',['Solve',['../classschwz_1_1Solve.html',1,'schwz::Solve&lt; ValueType, IndexType &gt;'],['../group__solve.html',1,'(Global Namespace)']]],
--- doc/event-based/search/variables_b.js
+++ doc/event-based/search/variables_b.js
@@ -3,5 +3,6 @@ var searchData=
   ['recv_5fbuffer',['recv_buffer',['../structschwz_1_1Communicate_1_1comm__struct.html#a78779301f90436a717cc8dcf50a5d717',1,'schwz::Communicate::comm_struct']]],
   ['remote_5fget',['remote_get',['../structschwz_1_1Communicate_1_1comm__struct.html#a39335cd2ee211817d66189fa6bff7d04',1,'schwz::Communicate::comm_struct']]],
   ['remote_5fput',['remote_put',['../structschwz_1_1Communicate_1_1comm__struct.html#a977cf8a2800021a50e59ee5cefb2a0ca',1,'schwz::Communicate::comm_struct']]],
-  ['reorder',['reorder',['../structschwz_1_1Settings.html#a429197e8093f3f87664ee6c37f91068e',1,'schwz::Settings']]]
+  ['reorder',['reorder',['../structschwz_1_1Settings.html#a429197e8093f3f87664ee6c37f91068e',1,'schwz::Settings']]],
+  ['restart_5fiter',['restart_iter',['../structschwz_1_1Settings.html#a7d26302ccb58fd96c795046ecc577694',1,'schwz::Settings']]]
 ];

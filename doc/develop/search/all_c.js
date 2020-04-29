b69dccaefaed13fcb7914beb8249d45f2b5ded26
GitHub Action <action@github.com>
2020-04-29 08:34:35 +0000

    Update documentation from 23c2d35

--- doc/develop/search/all_c.js
+++ doc/develop/search/all_c.js
@@ -4,5 +4,6 @@ var searchData=
   ['remote_5fget',['remote_get',['../structschwz_1_1Communicate_1_1comm__struct.html#a39335cd2ee211817d66189fa6bff7d04',1,'schwz::Communicate::comm_struct']]],
   ['remote_5fput',['remote_put',['../structschwz_1_1Communicate_1_1comm__struct.html#a977cf8a2800021a50e59ee5cefb2a0ca',1,'schwz::Communicate::comm_struct']]],
   ['reorder',['reorder',['../structschwz_1_1Settings.html#a429197e8093f3f87664ee6c37f91068e',1,'schwz::Settings']]],
+  ['restart_5fiter',['restart_iter',['../structschwz_1_1Settings.html#a7d26302ccb58fd96c795046ecc577694',1,'schwz::Settings']]],
   ['run',['run',['../classschwz_1_1SchwarzBase.html#ad4a01651b92f2bf44c4fedf8e1b9b33d',1,'schwz::SchwarzBase']]]
 ];

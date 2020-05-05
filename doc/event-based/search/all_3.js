46623f0dc278f8d9b294f8f1a9ebaea06d2c882e
GitHub Action <action@github.com>
2020-05-05 09:32:19 +0000

    Update documentation from ec94cb2

--- doc/event-based/search/all_3.js
+++ doc/event-based/search/all_3.js
@@ -9,7 +9,6 @@ var searchData=
   ['enable_5fonesided',['enable_onesided',['../structschwz_1_1Settings_1_1comm__settings.html#ad4273bd93cf3ee223c6c099a5fe9d02c',1,'schwz::Settings::comm_settings']]],
   ['enable_5foverlap',['enable_overlap',['../structschwz_1_1Settings_1_1comm__settings.html#a6ddd499f563bc4463e117b4e4ad495e6',1,'schwz::Settings::comm_settings']]],
   ['enable_5fput',['enable_put',['../structschwz_1_1Settings_1_1comm__settings.html#ad9b64e9c41ab52a3f58de7e190aabee0',1,'schwz::Settings::comm_settings']]],
-  ['enable_5frandom_5frhs',['enable_random_rhs',['../structschwz_1_1Settings.html#a04760b39513414af70498808c0a6b317',1,'schwz::Settings']]],
   ['error',['Error',['../classError.html',1,'Error'],['../classError.html#a146e7d37cbd73162b88a91ffa7fa776a',1,'Error::Error()']]],
   ['exchange_5fboundary',['exchange_boundary',['../classschwz_1_1Communicate.html#a40f61bcdae9d4d6a12091929944635a4',1,'schwz::Communicate::exchange_boundary()'],['../classschwz_1_1SolverRAS.html#a45aa596a569e246b5781af2dec84e9e7',1,'schwz::SolverRAS::exchange_boundary()']]],
   ['executor',['executor',['../structschwz_1_1Settings.html#a6920db9b39120b83399743083f17691f',1,'schwz::Settings']]],
--- doc/event-based/search/all_c.js
+++ doc/event-based/search/all_c.js
@@ -5,5 +5,6 @@ var searchData=
   ['remote_5fput',['remote_put',['../structschwz_1_1Communicate_1_1comm__struct.html#a977cf8a2800021a50e59ee5cefb2a0ca',1,'schwz::Communicate::comm_struct']]],
   ['reorder',['reorder',['../structschwz_1_1Settings.html#a429197e8093f3f87664ee6c37f91068e',1,'schwz::Settings']]],
   ['restart_5fiter',['restart_iter',['../structschwz_1_1Settings.html#a7d26302ccb58fd96c795046ecc577694',1,'schwz::Settings']]],
+  ['rhs_5ftype',['rhs_type',['../structschwz_1_1Settings.html#aba9fca6688555f4d5eb902ca5f0706f3',1,'schwz::Settings']]],
   ['run',['run',['../classschwz_1_1SchwarzBase.html#ad4a01651b92f2bf44c4fedf8e1b9b33d',1,'schwz::SchwarzBase']]]
 ];

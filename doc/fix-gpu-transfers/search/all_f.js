8db6491755e03a8e7dd5c649db74889748c35331
GitHub Action <action@github.com>
2020-05-17 10:59:56 +0000

    Update documentation from 44b71f6

--- doc/fix-gpu-transfers/search/all_f.js
+++ doc/fix-gpu-transfers/search/all_f.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['umfpackerror',['UmfpackError',['../classUmfpackError.html',1,'UmfpackError'],['../classUmfpackError.html#a3df702dc59370f9ff8f097f8657d3fff',1,'UmfpackError::UmfpackError()']]],
   ['update_5fboundary',['update_boundary',['../classschwz_1_1Communicate.html#aa1332376dfc67f5384527be90df7cbea',1,'schwz::Communicate::update_boundary()'],['../classschwz_1_1SolverRAS.html#a974f6e6be558338a37bdc65f34afdb26',1,'schwz::SolverRAS::update_boundary()']]],
+  ['updated_5fmax_5fiters',['updated_max_iters',['../structschwz_1_1Metadata.html#adabb7ff33b5002eebbdca546dadaefc4',1,'schwz::Metadata']]],
   ['use_5fprecond',['use_precond',['../structschwz_1_1Settings.html#ab51cf8eda75e05280084e89895121ca7',1,'schwz::Settings']]],
   ['utils',['Utils',['../structschwz_1_1Utils.html',1,'schwz::Utils&lt; ValueType, IndexType &gt;'],['../group__utils.html',1,'(Global Namespace)']]]
 ];
--- doc/fix-gpu-transfers/search/variables_e.js
+++ doc/fix-gpu-transfers/search/variables_e.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['updated_5fmax_5fiters',['updated_max_iters',['../structschwz_1_1Metadata.html#adabb7ff33b5002eebbdca546dadaefc4',1,'schwz::Metadata']]],
   ['use_5fprecond',['use_precond',['../structschwz_1_1Settings.html#ab51cf8eda75e05280084e89895121ca7',1,'schwz::Settings']]]
 ];

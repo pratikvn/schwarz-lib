5c737c21bf0c1868a7d9a14fa7c4b5a1d990d49f
GitHub Action <action@github.com>
2020-09-17 16:41:14 +0000

    Update documentation from 22e8171

--- doc/fix-twosided/search/variables_b.js
+++ doc/fix-twosided/search/variables_b.js
@@ -1,8 +1,6 @@
 var searchData=
 [
   ['recv_5fbuffer',['recv_buffer',['../structschwz_1_1Communicate_1_1comm__struct.html#a7b02126d598b9054b9adf0ebc9b3138c',1,'schwz::Communicate::comm_struct']]],
-  ['remote_5fget',['remote_get',['../structschwz_1_1Communicate_1_1comm__struct.html#a70f57e5c0ab445089ca9d8bdc0fa3838',1,'schwz::Communicate::comm_struct']]],
-  ['remote_5fput',['remote_put',['../structschwz_1_1Communicate_1_1comm__struct.html#a206d13b093699bf04bd1efb51c4290f2',1,'schwz::Communicate::comm_struct']]],
   ['reorder',['reorder',['../structschwz_1_1Settings.html#a429197e8093f3f87664ee6c37f91068e',1,'schwz::Settings']]],
   ['reset_5flocal_5fcrit_5fiter',['reset_local_crit_iter',['../structschwz_1_1Settings.html#af257396850c46e4d3217b4bb1a858114',1,'schwz::Settings']]],
   ['restart_5fiter',['restart_iter',['../structschwz_1_1Settings.html#a7d26302ccb58fd96c795046ecc577694',1,'schwz::Settings']]]
